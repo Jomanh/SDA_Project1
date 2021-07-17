7a5d62aa0e3b352180eb4d8e30c8bcaeeb3b633d
Jomanh <87065891+Jomanh@users.noreply.github.com>
2021-07-18 01:21:38 +0300

    Add files via upload

---
+++ RainbowSchoolSystem_SDA/RainbowSchoolSystem_SDA/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace RainbowSchoolSystem_SDA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" -------------------------------------------------------");
+            Console.WriteLine("|\t\t\t\t\t\t\t|");
+            Console.WriteLine("|\t\t   Rainbow School System     \t\t|");
+            Console.WriteLine("|\t\t\t\t\t\t\t|");
+            Console.WriteLine(" -------------------------------------------------------");
+
+            Teacher o = new Teacher();
+
+            int? choice = null;
+
+            while (choice != 4)
+            {
+                Console.Write("\nEnter Choice:\n [1: View Data  2: Add Data  3: Update  Data 4: Exit] ");
+
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Please enter valid choice");         
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        try
+                        {
+                            string data = o.View();
+                            Console.WriteLine(data);
+                            Console.WriteLine("Data View ended Successfully!");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(ex.Message);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+
+                    case 2:
+                        try
+                        {
+                            Console.Write("ID: ");
+                            int ID = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Name: ");
+                            string Name = Console.ReadLine();
+                            Console.Write("Class: ");
+                            string Class = Console.ReadLine();
+                            Console.Write("Section: ");
+                            string Section = Console.ReadLine();
+                            o.Add(ID, Name, Class, Section);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(ex.Message);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+
+                    case 3:
+                        try
+                        {
+                            Console.Write("Enter ID You Want To Update:");
+                            int searchID = int.Parse(Console.ReadLine());
+                            Teacher tData = o.Search(searchID);
+
+                            if (tData != null)
+                            {
+                                Console.Write("New ID: ");
+                                int newID = Convert.ToInt32(Console.ReadLine());
+                                Console.Write("New Name: ");
+                                string newName = Console.ReadLine();
+                                Console.Write("New Class: ");
+                                string newClass = Console.ReadLine();
+                                Console.Write("New Section: ");
+                                string newSection = Console.ReadLine();
+                                o.Update(searchID, newID, newName, newClass, newSection);
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine(searchID + " Does Not Exist!");
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(ex.Message);
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+
+                    case 4:
+                        break;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Not a valid choice!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                }
+            }
+
+            Console.WriteLine("Have a nice day!");
+        }
+    }
+}
---
+++ RainbowSchoolSystem_SDA/RainbowSchoolSystem_SDA/Teacher.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace RainbowSchoolSystem_SDA
+{
+    class Teacher
+    {
+        string path = @"C:..\TeacherData.txt";
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Section { get; set; }
+
+        public Teacher()
+        {
+
+        }
+
+        public Teacher(int ID, string Name, string Class, string Section)
+        {
+            this.ID = ID; this.Name = Name; this.Class = Class; this.Section = Section;
+        }
+
+        public string View()
+        {
+            StreamReader reader = new StreamReader(path);
+            string data = reader.ReadToEnd();         
+            return data;
+        }
+
+        public void Add(int ID, string Name, string Class, string Section)
+        {
+            StreamWriter writer = File.AppendText(path);
+            string value = ID + ", " + Name + ", " + Class + ", " + Section + "\n";
+
+            writer.Write(value);
+            writer.Close();
+
+            Console.WriteLine("Data Added Successfully!");
+        }
+
+        public void Update(int searchID, int ID, string Name, string Class, string Section)
+        {
+            string[] lines = File.ReadAllLines(path);
+            bool status = false;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] line = lines[i].Split(",");
+
+                if (int.Parse(line[0]) == searchID)
+                {
+                    lines[i] = ID + ", " + Name + ", " + Class + ", " + Section;
+                    status = true;
+                }
+            }
+            if (status)
+            {
+                File.WriteAllLines(path, lines);
+                Console.WriteLine("Data Updated Successfully!");
+            }
+        }
+
+        public Teacher Search(int Id)
+        {
+            string[] lines = File.ReadAllLines(path);
+
+            foreach (string s in lines)
+            {
+                string[] line = s.Split(",");
+
+                if (int.Parse(line[0]) == Id)
+                {
+                    Teacher data = new Teacher(int.Parse(line[0]), line[1], line[2], line[3]);
+                    return data;
+                }
+            }
+
+            return null;
+        }
+    }
+}
